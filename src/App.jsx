144f214d3feed8fbedd7e8daa4c33ece8e0ca609
keieng <105902831+keieng@users.noreply.github.com>
2022-12-11 20:34:23 +0900

    ノート追加、削除機能実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 import { useState } from "react";
 import { Col, Row } from "react-bootstrap";
+import uuid from "react-uuid";
 import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: "This one is a title",
+      content: "This one is a text",
+      createDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  };
+
+  const onDeleteNote = (id) => {
+    const filterNotes = notes.filter((note) => note.id !== id);
+    setNotes(filterNotes);
+  };
   return (
     <div className="App">
-      <Row>
+      <Row className="g-0">
         <Col>
-          <Sidebar />
+          <Sidebar
+            onAddNote={onAddNote}
+            notes={notes}
+            onDeleteNote={onDeleteNote}
+          />
         </Col>
         <Col>
           <Main />
